cc66cd118bf5bd30580e78495983a5d391e13a30
saibork100 <trikimahmoud86@gmail.com>
2024-04-10 17:56:22 +0100

    create funtion number entery

--- test.py
+++ test.py
@@ -14,14 +14,41 @@ calc.grid()       # Add the frame to the window using the grid geometry manager
 class Calc():
     def __init__(self):
         # Initialize the total attribute to 0
-        self.total = 0
+        self.total=0
         # Initialize the current attribute to an empty string
-        self.current = ''
+        self.current=''
         # Initialize the input_value attribute to True
-        self.input_value = True
+        self.input_value=True
         # Initialize the check_sum attribute to False
-        self.check_sum = False
+        self.check_sum=False
         # Initialize the op attribute to an empty string
-        self.op = ''
+        self.op=''
         # Initialize the result attribute to False
-        self.result = False
+        self.result=False
+
+    def numberEnter(self, num):
+        # set the result attridute to False
+        self.result=False
+        # Get the current value display on the calculator's display 
+        firstnum=txtDisplay.get()
+        # Convert the input number to a string
+        secondnum=str(num)
+
+        # Check if the input_value attribute is True
+        if self.input_value:
+               # If it is, set the current attribute to the input number
+              self.current = secondnum
+              # Set the input_value attribute to False
+              self.input_value=False
+        else:
+             # If the input_value attribute is False, check if the input number is a decimal point
+            if secondnum == '.':
+                # If it is, check if there is already a decimal point in the current value
+                if secondnum in firstnum:
+                    # If there is, return without doing anything
+                    return
+            # If the input number is not a decimal point, or if there is no decimal point in the current value, concatenate the current value and the input number    
+            self.current = firstnum+secondnum
+        # Display the current value on the calculator's display
+        self.display(self.current)
+        
