f4ff8bb49bfcdf6c692fc46a67db9f63fd39d945
saibork100 <trikimahmoud86@gmail.com>
2024-04-10 17:13:32 +0100

    create frame

--- test.py
+++ test.py
@@ -2,3 +2,11 @@
 from tkinter import *
 import math
 import tkinter.messagebox
+
+root = Tk()
+root.title("sciebfic calculator")
+root.comfiguer (bg="black", fg='white')
+root.resizable(width=False, height=False)
+root.geometry("480x568+450+90")
+calc =Frame(root)
+calc.grid()
